a06a132337447e234a6c65699eba1ac2f7fd158d
Aftab <aftabrafi9@gmail.com>
2024-03-02 15:03:41 +0600

    registration views done

--- App_Login/views.py
+++ App_Login/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import CustomUserCreationForm, CustomAuthenticationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+def registration(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'App_Login/register.html', {'form': form})
+
+
+def login_page(request):
+    if request.method == 'POST':
+        form = CustomAuthenticationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = CustomAuthenticationForm()
+    return render(request, 'App_Login/login.html', {'form': form})
+
+
+@login_required
+def user_profile(request):
+    return render(request, 'App_Login/user_profile.html')
